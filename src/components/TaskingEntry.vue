2efa258c4b3d07be36dd14cc3bba4ce8b34225df
cow <cow-co@users.noreply.github.com>
2022-03-18 23:04:10 +0000

    A Basic Tasking Entry Layout

--- src/components/TaskingEntry.vue
+++ src/components/TaskingEntry.vue
@@ -1,7 +1,9 @@
 <template>
-  <li class="tasking-entry">
-    <p>{{ server }}</p>
-  </li>
+  <div class="tasking-entry">
+    <p>Task: {{ summary }}</p>
+    <p>Server: {{ server }}</p>
+    <p>Location: {{ location }}</p>
+  </div>
 </template>
 
 <script>
@@ -10,3 +12,15 @@ export default {
   props: ["summary", "location", "server"],
 };
 </script>
+
+<style scoped>
+.tasking-entry {
+  background-color: gray;
+  width: 50%;
+  margin-left: 25%;
+  margin-bottom: 20px;
+  min-height: 65px;
+  border-radius: 5px;
+  padding: 10px 5px;
+}
+</style>
