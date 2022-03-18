ee8c4b983925d3628a1054b9ae0391f220830408
cow <cow-co@users.noreply.github.com>
2022-03-18 22:44:13 +0000

    Make a Very Basic Layout

---
+++ src/components/TaskingEntry.vue
@@ -0,0 +1,11 @@
+<template>
+  <li class="tasking-entry">
+    Tasking
+  </li>
+</template>
+
+<script>
+export default {
+  name: "TaskingEntry"
+}
+</script>
---
+++ src/components/pages/QueuePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="queue">
+    <h1>Current Tasks</h1>
+    <ul class="queue-list">
+      <tasking-entry />
+    </ul>
+    <button class="progress-button" v-on:click="this.openCreateWindow()">Create Tasking</button>
+  </div>
+</template>
+
+<script>
+import TaskingEntry from "../TaskingEntry.vue";
+
+export default {
+  components: { TaskingEntry },
+  name: "QueuePage"   ,
+  methods: {
+    openCreateWindow() {
+      console.log("Create Tasking");
+    }
+  }
+}
+</script>
